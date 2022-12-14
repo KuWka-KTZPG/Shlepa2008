f1a6edff7ae9f4b4625209305debe8975e57ef97
Каллас Даня 142 <114554646+KuWka-KTZPG@users.noreply.github.com>
2022-12-14 12:02:38 +0700

    Add files via upload

---
+++ familie.py
@@ -0,0 +1,32 @@
+#мальчик 1
+def f(familie):
+    print(familie[::-1])
+#мальчик 2
+def g(pol, familie):
+    mal_2=pol +" "+ (familie[::-1])
+    return mal_2
+
+
+def u(familie):
+    fam_glas=list('уеаоэяию')
+    for i in familie:
+        if i in fam_glas:
+            print(i)
+        
+
+
+
+
+    
+#прога
+familie=input("Напшите фамилию: ")
+f(familie)
+print("////////")
+pol=input("ведите пол мальчика: ")
+print(g(pol , familie))
+print("Гласные: ")
+u(familie)
+
+
+
+
