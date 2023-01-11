9d751f11cc4b7e4a921e23e635e96a58eba70389
Каллас Даня 142 <114554646+KuWka-KTZPG@users.noreply.github.com>
2023-01-11 11:59:03 +0700

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -8,3 +8,16 @@ for N in range(516):
     if int(b, 2) > 516:
         print(N)
         break
+
+def f6():
+    from turtle import *
+    left(90)
+    for i in range(7):
+        forward(300)
+        right(120)
+    pu()
+    for x in range(1,9):
+        for y in range(1,10):
+            goto(x*30,y*30)
+            dot(5)
+    done()
