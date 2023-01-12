770564ab189c8ac5138511a252a2946dba75b664
Каллас Даня 142 <114554646+KuWka-KTZPG@users.noreply.github.com>
2023-01-12 11:39:18 +0700

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -21,3 +21,17 @@ def f6():
             goto(x*30,y*30)
             dot(5)
     done()
+def f8():
+    from itertools import product
+    nums=product('01234567', repeat=5)
+    k=0
+    n='16 36 56 76 61 63 65 67'
+    nn=n.split()
+    for n in nums:
+        numb=''.join(n)
+        sp=[]
+        if numb.count('6')==1 and numb:
+            sp.append(x)
+        if not sp:
+            k+=1
+    print(k)
